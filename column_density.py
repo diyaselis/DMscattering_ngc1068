7a73d0211caabbc01451a7bdf6437d4e12b521d9
diyaselis <diyaselis@gmail.com>
2023-02-10 16:01:13 -0500

    Update column_density.py

--- column_density.py
+++ column_density.py
@@ -1,90 +1,83 @@
 import numpy as np
 from scipy import integrate
 
+from astropy import units as u
+from astropy.coordinates import SkyCoord,EarthLocation, Galactic, AltAz, ICRS, get_icrs_coordinates
+import astropy.coordinates as coord
+from astropy.time import Time
+
 # natural units
+eV = 1.0
 GeV = 1.0e9
 MeV = 1.0e6
 keV = 1.0e3
 second = 1.5192669e-17 # s to eV
-meter = 5.06773093741e6 # m to 1/eV (hbar c)
-cm = 1.0e-2*meter # cm to eV
-km = 1.0e3*meter # km to eV
+meter = 5.06773093741e6 # m to 1/eV
+cm = 1.0e-2*meter # cm to 1/eV
 kg = 5.62e35 # kg to eV
-gr = 1e-3*kg  # kg to eV
+gr = 1e-3*kg # g to eV
 Na = 6.0221415e+23
+hbarc = 3.16152677e-26
 parsec = 3.085678e13 # pc to km
-kpc = 1.0e3*parsec # kpc to km
-Mpc = 1.0e6*parsec
-
+kpc = 1e3 * parsec * 1e5 # kpc to cm
+Mpc = 1e6 * parsec
 M_sun = 2e30 #kg
+
 
 # NGC 1068: Basic Details
 ra_ngc = np.radians(40.6696292) # 2.7113056
 dec_ngc = np.radians(-0.0132806) # -0.0133333
 z_ngc = 0.003793
-M_ngc = 1e8 * M_sun
+M_ngc = 2.7*1e10 * M_sun # kg (M_halo)
 # =============== Part 1: Galactic contribution =================
 
+# definition of constants
+r0 =  8.127 * kpc # distance from the Sun to GC [kpc]; convert kpc to cm
+rs = 26 * kpc     # scale radius [kpc]; convert kpc to cm
+r_halo = 30 * kpc # maximum Halo radius [kpc]; convert kpc to cm;
+rho_0 = 0.4             # local densitity [GeV/cm^-3]
+gamma = 1.0             # slope parameter
 
-# Values based on Iooco paper https://arxiv.org/pdf/1901.02460.pdf
-RS = 26*(kpc*1e5)  # kpc -> cm
-gamma=1 # standard NFW
-rho_s=1e7*(M_sun*1e3)/(kpc*1e5)**3 # 1e7 M sun/kpc^3 -> g/cm^3
-R=8.3*(kpc*1e5) # Distance from sun to galactic centre kpc -> cm
+# calculating scale densitity rho_s [GeV/cm^-3]
+rho_s = rho_0 / ((2**(3 - gamma))/(((r0/rs)**gamma)*(1 + (r0/rs))**(3 - gamma))) # [GeV/cm^-3]
 
+# calculating upper limit of line of sight (los)
+psi = 0                 # angle between GC and los [rad]
+xmax = np.sqrt(r_halo**2 - (r0**2)*(np.sin(psi)**2)) + r0*np.cos(psi) # upper limit of los integration or change to inf
 
-def get_angle(ra,dec):
-    # ra radians from 0 to 2pi
-    # dec radians from -pi/2 to pi/2
-    dec_G=27.12825*np.pi/180
-    ra_G=192.85948*np.pi/180
-    l_NCP=122.93192*np.pi/180
+# galactic center definition
+gc = get_icrs_coordinates('Galactic Center')
+los_ngc = SkyCoord(ra_ngc * u.rad, dec_ngc * u.rad, frame='icrs')
+psi_ngc = gc.separation(los_ngc).radian
 
-    b = np.arcsin(np.sin(dec)*np.sin(dec_G)+np.cos(dec)*np.cos(dec_G)*np.cos(ra-ra_G))
-    if dec<0:
-        b += np.pi
+def rho_DM(x):
+    '''
+    Parameters
+    ----------
+    r: galactocentric distance [cm]
 
-    l = l_NCP-np.arcsin(np.cos(dec)*np.sin(ra-ra_G)/np.cos(b))
-    theta=np.arccos(np.cos(b)*np.cos(l))
-    return theta
+    Returns
+    -------
+    rho_DM: DM densitity in equatorial coordinates [GeV/cm^-3]
+    '''
+    # calculating DM densitity rho_DM [GeV/cm^-3] based on eq. 5
+    r = np.sqrt(r0**2 - 2*x*r0*np.cos(psi_ngc) + x**2)
+    rho_DM = rho_s * (2**(3 - gamma))/(((r/rs)**gamma)*(1 + (r/rs))**(3 - gamma)) # [GeV/cm^-3]
+    return rho_DM
 
+def get_t_NFW():
+    """ Returns the NFW column density.
 
-def rho_NFW(theta, x):
-    """ Returns the NFW density in gr/cm^3.
-
-    Args:
-	theta: angle to galactic centre (radians)
-	x: distance from Earth to source (km)
-	r: distance from galactic centre to source (km)
-
-    Returns:
-        rho: density in gr/cm^3
+    Returns
+    -------
+        t: column density in GeV/cm^2
     """
-    r = np.sqrt(x ** 2 + R ** 2 - 2 * x * R * np.cos(theta))
-    rho=rho_s/((r/RS)**gamma*(1+r/RS)**(3-gamma))
-    return rho
-
-
-def get_t_NFW(ra,dec):
-    """ Returns the NFW column density for a given zenith angle.
-
-    Args:
-        theta: zenith angle in radians.
-
-    Returns:
-        t: column density in g/cm^2
-    """
-    # x: distance from Earth to source
-
-    xmax =  RS #np.sqrt((RS - d) ** 2 * np.cos(theta) ** 2 + d * (2 * RS - d)) - (RS - d) * np.cos(theta)
-    theta = get_angle(ra,dec)
-
-    n = lambda x: rho_NFW(theta, x)  # mass density
-    t=integrate.quad(lambda x: n(xmax - x), 0, xmax, epsrel=1.0e-3, epsabs=1.0e-18)[0] # g/cm^2
+    n = lambda x: rho_DM(x) # mass density
+    t = integrate.quad(lambda x: n(xmax - x), 0, xmax, epsrel=1.0e-3, epsabs=1.0e-18)[0]   # GeV/cm^2
     return t
 
-column_dens_1 = get_t_NFW(ra_ngc,dec_ngc) * gr * Na /cm**2  # g/ cm^2 -> eV^3
-print(column_dens_1)
+column_dens_1 = get_t_NFW()  # GeV/cm^2
+# print('CD #1: {:.4e} GeV / cm^2'.format(column_dens_1))
 
 
 # ================= Part 2: Cosmological background ================
@@ -95,36 +88,47 @@ omega_m = 0.3175
 omega_lambda = 0.6825
 omega_dm = 0.2685
 # crit_dens = 5.6e-6 # GeV cm^-3
-G = 6.67*1e-20 # km^3 s^-2 kg^-1
-crit_dens = 3*hubble_const**2 / (8*np.pi*G) * gr * Na /km**3 # kg km^-3 -> eV^4
+G = 6.67*1e-20 * (1e15) * (GeV/kg) # km^3 s^-2 kg^-1 -> cm^3 s^-2 GeV^-1
+crit_dens = 3*hubble_const**2 / (8*np.pi*G) # GeV / cm^3
 
 hubble_param = lambda z: hubble_const * (omega_m*(1+z)**3 + omega_lambda)**(1/2) # removed radiation term
-column_dens_2 = omega_dm*crit_dens * integrate.quad(lambda z: 1/hubble_param(z),0,np.infty)[0] / second # eV^4 s^-1 to eV^3
-print(column_dens_2)
+column_dens_2 = omega_dm*crit_dens * integrate.quad(lambda z: 1/hubble_param(z),0,z_ngc)[0] / (1e2) # GeV * s / cm^3 to GeV / cm^2 taking c=1
+# print('CD #2: {:.4e} GeV / cm^2'.format(column_dens_2))
 
 # ================= Part 3: NGC 1068 DM Halo density ==================
+v_vir = 1068 * (1e5) # km / s -> cm / s
+v_rot = 310 * (1e5) # km / s -> cm / s
+# v_rot = 410 * (1e5) # km / s -> cm / s
+h = hubble_const / (100/Mpc) # dimensionless
+# planck_length = 1.616 * 1e-35 # m
+# lambda_const = 2.888 * 1e-122 /(planck_length)**2 #
+# omega_0 = 1.0 - lambda_const
+omega_0 = rho_0 / crit_dens
+omega_z0 = lambda z: 1 + z
+r_vir = 1.63e-2 * (M_ngc*h/M_sun)**(1/3) * (omega_0/omega_z0(z_ngc))**(-1/3) * (1+z_ngc) / h # kpc
+print(r_vir)
+r_vir = G*M_ngc / (v_vir**2) # cm
+print(r_vir)
 
-# h = H_0 / 100
-# omega_0 = 1.0
-# omega_z0 = lambda z: 1 + z
-# r_vir = 1.63e-2 * (M_ngc*h/M_sun)**(1/3) * (omega_0/omega_z0(z_ngc))**(-1/3) * (1+z_ngc)
-# M_vir = (4*np.pi*200/3)*crit_dens_ngc*r_vir**3
-# r_s = (3*M_vir/(4*np.pi*200*crit_dens_ngc))**(1/3) / c
+# M_vir = (4*np.pi*200/3)*crit_dens*(r_vir * kpc)**3 # GeV
+# print(M_vir)
+# M_vir = 2.7*1e10 * M_sun # kg (M_halo)
+# print(M_vir)
 
-
-M_vir = 2.7*1e10 * M_sun # kg (M_halo)
-v_vir = 410 # km / s
-r_vir = G*M_vir / (v_vir**2) * km # km -> eV
+quit()
 
 c = 11.7*(M_vir/(M_sun*1e11))**(-0.075)
 delta_c = (200*c**3)/(3*(np.log(1+c)-c/(1+c)))
+# print('r_vir', r_vir)
+r_s = r_vir / c # cm
+# print(r_s)
+r_s = (3*M_vir/(4*np.pi*200*crit_dens))**(1/3) / c # cm
+# print(r_s)
 
-r_s = r_vir / c # eV
-
-rho_ngc = lambda r: delta_c * crit_dens / ((r/r_s)*(1+r/r_s)**2)
-column_dens_3 = integrate.quad(lambda r: rho_ngc(r),0,np.infty)[0] # infinity or radius of galaxy
-print(column_dens_3)
+rho_ngc = lambda r: delta_c * crit_dens / ((r/r_s)*(1+r/r_s)**2) # GeV / cm^3
+column_dens_3 = integrate.quad(lambda r: rho_ngc(r),0,np.infty, epsrel=1.0e-3, epsabs=1.0e-18)[0] # infinity or radius of galaxy
+print('CD #3: {:.4e} GeV / cm^2'.format(column_dens_3))
 
 # ================
 total_col_dens = column_dens_1 + column_dens_2 + column_dens_3
-print('total_col_dens = ',total_col_dens)
+print('total_col_dens = {:.4e} GeV/ cm^2'.format(total_col_dens))
